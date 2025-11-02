5497c4832c89925531854599763fb6cded26b4cb
azertypow <nico@villa1203.ch>
2025-11-02 23:32:59 +0100

    feat: add content and all components

--- app/app.vue
+++ app/app.vue
@@ -2,20 +2,222 @@
   <div class="v-index"
   >
     <header class="v-index__header">
-      <img class="v-index__header__logo" src="/img.png" alt="logo" />
+      <img class="v-index__header__logo" src="/images/logo.svg" alt="logo" />
     </header>
     <main class="v-index__main">
-      <h1 class="v-index__main__title">Résidence COLECTIVA</h1>
+      <div class="v-index__main__intro">
+        <p>
+          <strong>Colectiva</strong> est un programme de résidences de recherche artistique
+          installé au cœur d’un <strong>lieu de vie collectif</strong> à Genève,
+          au 6 chemin Annie&#8209;Jiagge. Il accueille des artistes suisses, régionaux et internationaux
+          pour des <strong>séjours de 2 à 8 semaines</strong>, dans un environnement propice
+          à la <strong>réflexion</strong>, à la <strong>création</strong> et au <strong>partage</strong>.En collaboration avec des
+          partenaires issus des <strong>milieux sociaux, culturels et scientifiques,</strong>
+          Colectiva cultive un esprit de <strong>convivialité</strong>, d’<strong>écoute</strong> et de <strong>coopération</strong>,
+          nourri par la <strong>diversité des parcours et des identités.</strong>
+        </p>
 
+        <p>
+          Le programme s’articule autour de <strong>quatre axes de recherche et de
+          création&nbsp;:</strong>
+        </p>
+        <p>
+          <strong>→ Médiation culturelle et pratiques collaboratives</strong>
+          <br>Initiatives favorisant les rencontres entre artistes, publics et disciplines
+          variées, encourageant la recherche partagée et enrichissant le dialogue
+          des savoirs collectifs.
+        </p>
+        <p>
+          <strong>→ Récits, mémoire et transmission</strong>
+          <br>Projets qui explorent les histoires et les traces inscrites dans
+          l’environnement de Colectiva, stimulant la conversation entre passé et
+          présent.
+        </p>
+        <p>
+          <strong>→ Transitions culturelles et écologiques</strong>
+          <br>Exploration des liens entre héritages coloniaux, diasporas et transition
+          écologique, interrogeant leur influence sur les cultures, les mobilités et les
+          perceptions contemporaines de la durabilité et du changement.
+        </p>
+        <p>
+          <strong>→ Architecture, urbanisme et design</strong>
+          <br>Projets en relation avec les espaces publics et leurs habitant·e·x,
+          questionnant la cohabitation et l’aménagement territorial.
+        </p>
+        <p>
+          Ouvert à toutes les disciplines artistiques et de recherche, y compris
+          aux pratiques liées aux arts sonores, Colectiva est un espace vivant
+          oùse tissent les savoirs, les expériences et les imaginaires pour inventer,
+          ensemble, de nouvelles manières d’investir le monde.
+        </p>
+      </div>
       <div class="v-index__main__projects">
-        <AppProject title="Association Nébuleuse">
-          <p>COLECTIVA est un programme de résidences orienté vers les domaines des arts visuels et sonores qui a débuté
-            en 2022.</p>
-          <p>COLECTIVA s’engage dans des réflexions sur la vie en communauté et a pour but de créer des liens entre les
-            différentes structures culturelles du canton de Genève. COLECTIVA veut aussi visibiliser des artistes
-            travaillant en Amérique Latine et les mettre en lien avec la Suisse pour stimuler les échanges culturels entre
-            ces deux espaces.</p>
-          <img src="/images/projects/association_nebuleuse/img.png" alt="image du couverture" />
+        <AppProject
+          title="Résidence Novembre 2025"
+          cover="images/projects/251024felixKubinresidence.jpg"
+        >
+          <p>
+            Colectiva a le plaisir d’annoncer la résidence de
+            recherche et de création — urbaine et collective —
+            de Felix Kubin, compositeur et artiste sonore allemand
+            dont l’oeuvre se situe à la croisée de la pop futuriste, de
+            l’électroacoustique et de la musique contemporaine.
+            Reconnu pour l’originalité de ses pièces radiophoniques
+            ainsi que pour ses compositions pour le cinéma, le
+            théâtre et l’orchestre, il poursuit une quête constante
+            d’innovation et d’expérimentation sonore.
+          </p>
+          <p>
+            Invité par Colectiva, Felix Kubin mène pendant quatre
+            semaines une recherche sonore in situ, prenant pour
+            point d’ancrage la polysémie du terme “chemin”. Sa
+            démarche s’inscrit dans un processus dynamique,
+            mêlant exploration d’archives de cassettes, entretiens
+            et captation des résonances propres aux espaces
+            investis.
+          </p>
+          <p>
+            En dialogue étroit avec le contexte culturel et social
+            régional, cette recherche s’ouvre à des trajectoires
+            sonores inattendues, où pourront émerger des pistes
+            d’insurrection et d’utopie.
+          </p>
+          <p style="text-transform: uppercase; font-size: 1.25rem">
+            Découvrez les événements
+            programmés dans le cadre de cette
+            résidence !
+          </p>
+        </AppProject>
+
+        <AppProject info="9 novembre, 17h, Le Passage :"
+                    type="Conférence"
+                    title="Deutsche Kassettentäter / German Tape Delinquents"
+                    cover="images/projects/conférence_felix_kubin.jpg"
+                    :has_toggle="true"
+        ><p>
+          <strong>Nous avons le plaisir de vous inviter à la conférence de Felix Kubin</strong>,
+          qui présentera un panorama de la scène allemande de l’enregistrement
+          maison sur cassette entre 1981 et 1993.
+        </p>
+          <p>
+            À la fin des années 1970, la scène musicale allemande a été secouée par
+            une explosion fulgurante de nouveaux groupes, au son radicalement
+            différent de tout ce qui existait auparavant. Ces artistes jouaient une
+            musique expérimentale, ludique, absurde, minimaliste et inventive,
+            avec des paroles chantées — ou plus souvent hurlées — exclusivement en
+            allemand.
+          </p>
+          <p>
+            De petites maisons de disques comme ZickZack et Ata Tak
+            commencèrent à publier les enregistrements de ces jeunes artistes qui
+            se multipliaient rapidement. C’est ainsi que le label indépendant est né.
+          </p>
+          <p>
+            L’esprit de ce mouvement — plus tard nommé Neue Deutsche Welle
+            — est né de la collision entre l’attitude confiante et revendicative du
+            punk, l’héritage de l’avant-garde électronique allemande et la croyance
+            en une apocalypse atomique imminente. La présentation s’appuie
+            notamment sur des entretiens avec Alfred Hilsberg (fondateur du label
+            ZickZack) et Frank Apunkt Schneider (auteur de Als die Welt noch
+            unterging / When the world was still on the verge of downfall).
+          </p>
+          <p>
+            Rendez-vous le dimanche 9 novembre à 17h au Passage pour découvrir
+            cette époque fascinante de la musique expérimentale allemande et
+            échanger autour de son héritage.
+          </p>
+          <p>
+            Nous nous réjouissons de vous y retrouver !
+          </p>
+
+        </AppProject>
+
+
+        <AppProject title="Demo Dandies"
+                    type="Soirée DJ & émission radio expérimentale"
+                    info="15 novembre, Ressources Urbaines, Ancienne Poste des Charmilles :"
+                    cover="images/projects/251022demoDandies-affiche.jpg"
+                    :has_toggle="true"
+        >
+          <h3>Bar opérationnel et Drunch préparé par Sabi, Lujo popular</h3>
+
+          <p>Nous avons le plaisir de vous inviter à Demo Dandies, un événement
+          unique qui mêle DJ set expérimental, émission radio en direct et
+          exploration sonore. Après Leipzig, Luxembourg, Halle, Berlin et
+          Hambourg, Demo Dandies pose ses valises et micros à Genève –
+            réjouissance !</p>
+
+          <h3>Qu’est-ce que Demo Dandies ?</h3>
+
+          <p>Demo Dandies est un espace dédié à l’expérimentation et à la
+          spontanéité. Sur scène, Felix Kubin & Felix Raeithel (Istari Lasterfahrer)
+          animent une émission radio en direct, diffusant et commentant les démos
+          musicales envoyées ou apportées par le public. Ces enregistrements ne
+          sont pas des morceaux finalisés : ce sont des brouillons sonores, bruts et
+            inédits, où l’imperfection fait partie de la création.</p>
+
+          <h3>Qu’est-ce qu’une démo ?</h3>
+
+          <p>Une démo (ou maquette) est un enregistrement non finalisé, qui capture</p>
+          l’idée originale d’un morceau avant sa version définitive. Demo Dandies
+          met en lumière la créativité brute et l’expérimentation, là où l’originalité
+          prime sur la perfection.
+
+          <h3>Pourquoi venir ?</h3>
+
+          <p>→ Écouter des sons rares et originaux, parfois désordonnés, toujours</p>
+          fascinants.
+          → Participer activement en apportant vos propres démos : CD, cassette,
+          MP3, matériel inédit.
+          → Vivre une expérience collective, entre danse, découverte et échanges
+          dans une ambiance conviviale et chaleureuse.
+
+          <h3>Programme</h3>
+
+          <p>19h00 : Ouverture des portes</p>
+          20h00 – 23h00 : Émission Demo Dandies en direct
+          23h00 – 02h00 : Suite dansante
+
+          <h3>Comment participer ?</h3>
+
+          <p>→ Supports acceptés : CD-R audio, cassette ou clé USB (nommer vos</p>
+          fichiers : NomArtiste_Titre Morceau.wav + document texte avec vos
+          coordonnées).
+          → Limite : 3 morceaux inédits maximum.
+          → Formats numériques (wav, aif 16 bits / 44,1 kHz) uniquement en cas
+          d’impossibilité de remise en personne, via swisstransfer à info@
+          demodandies.de
+          → Évitez les liens vers Soundcloud ou autres plateformes.
+          → Dates limites :
+          Depuis la Suisse : avant vendredi 14 novembre 2025
+          → RU – Ancienne Poste des Charmilles / mention Demo Dandies
+          23 rue des Charmilles
+          1203 Genève, Suisse
+          Depuis l’étranger : avant 10 novembre 2025 à :
+          → Felix Raeithel
+          3plusX
+          Gaußstrasse 25b
+          22765 Hambourg, Allemagne
+
+          <p><strong>Les démos livrées en personne seront privilégiées. Après chaque
+          événement, une compilation des morceaux préférés est publiée sur
+            cassette et Bandcamp.</strong></p>
+
+          <p>+ d’infos :</p>
+          demodandies.de
+
+          <p>Pour toute question, contactez-nous à</p>
+
+          <p>info@demodandies.de ou projet@colectiva.ch</p>
+        </AppProject>
+
+
+        <AppProject title="Sortie de résidence"
+                    info="26 novembre, 21h30, Cave 12 :"
+                    cover="images/projects/251024felixKubin-cave12-digital.jpg"
+                    :has_toggle="true"
+        >
+          + d'infos : <a href="https://www.cave12.org/felix-kubins-colectiva/" >cave12.org/felix-kubins-colectiva/</a>
         </AppProject>
       </div>
 
@@ -27,31 +229,49 @@
 <style lang="scss" scoped>
 .v-index {
   padding: 2rem;
+  display: flex;
+  flex-direction: column;
 }
 
 .v-index__header {
   display: flex;
-  align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
+  margin-left: calc(100% / 12 * 4);
+  width: calc(100% / 12 * 8);
+  max-width: 20rem;
+  padding-bottom: 1rem;
+
+  @media (max-width: 1000px) {
+    margin-left: 0;
+  }
 }
 
 .v-index__header__logo {
-  height: 1rem;
+  height: 4rem;
+
+  @media (max-width: 1000px) {
+    width: 100%;height: auto;
+  }
 }
 
 .v-index__main {
-  max-width: 40rem;
+  max-width: 72rem;
   margin-left: auto;
   margin-right: auto;
 }
 
-.v-index__main__projects {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+.v-index__main__intro {
+  max-width: 40rem;
+  width: calc(100% / 12 * 8);
+  margin-left: calc(100% / 12 * 4);
 
+  @media (max-width: 1000px) {
+    width: calc( ((100% + var(--gutter)) / 12 * 12) - var(--gutter));
+    margin-left: 0;
+  }
+}
 </style>
+
 <script setup lang="ts">
 import AppProject from "~/assets/components/AppProject.vue";
 </script>
--- app/assets/components/AppProject.vue
+++ app/assets/components/AppProject.vue
@@ -1,9 +1,27 @@
 <template>
     <section class="v-app-project"
+             :class="{ 'v-app-project--has-toggle': has_toggle }"
     >
-      <h2 class="v-app-project__title">{{ title }}</h2>
+      <div class="v-app-project__aside">
+        <img class="v-app-project__cover"
+             v-if="cover && has_toggle"
+             :src="cover"
+             :alt="`image de couverture de ${title}`"
+        >
+      </div>
       <div class="v-app-project__content">
-        <slot />
+        <h2 class="v-app-project__content__title"
+        >
+          <span v-if="info" style="font-weight: normal">{{ info }}</span>
+          {{ title }}
+          <span v-if="type" style="font-weight: normal">— {{ type }}</span>
+        </h2>
+        <img class="v-app-project__cover"
+             v-if="cover && !has_toggle"
+             :src="cover"
+             :alt="`image de couverture de ${title}`"
+        >
+        <slot/>
       </div>
     </section>
 </template>
@@ -16,7 +34,11 @@
 import { defineProps } from 'vue'
 
 defineProps<{
-    title: string
+  title: string
+  info?: string
+  type?: string
+  has_toggle?: boolean
+  cover?: string
 }>()
 </script>
 
@@ -27,12 +49,48 @@ defineProps<{
 <style lang="scss" scoped >
 .v-app-project {
   width: 100%;
+  color: #109d34;
+  border-top: solid 2px #109d34;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+
+  --gutter: 1rem;
 }
 
-:global(.v-app-project__content img) {
+.v-app-project__aside {
+  width: calc( ((100% + var(--gutter)) / 12 * 4) - var(--gutter));
+
+    @media (max-width: 1000px) {
+      width: calc( ((100% + var(--gutter)) / 12 * 12) - var(--gutter));
+    }
+}
+
+.v-app-project__cover {
   display: block;
-  width: 100%;
   height: auto;
+  width: 100%;
+  max-height: 95vh;
+  object-fit: contain;
+  object-position: left;
+  margin-bottom: 1rem;
+
+  .v-app-project--has-toggle & {
+    position: sticky;
+    top: 1rem;
+    border: solid 2px #109d34;
+    margin-top: 1rem;
+    max-height: none;
+  }
+}
+
+.v-app-project__content {
+  width: calc( ((100% + var(--gutter)) / 12 * 8) - var(--gutter));
+  max-width: 40rem;
+
+  @media (max-width: 1000px) {
+    width: calc( ((100% + var(--gutter)) / 12 * 12) - var(--gutter));
+  }
 }
 
 </style>
